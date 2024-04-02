ddc680f5bbc39ba0ca6fe1151fcf2a4f92c2bae3
Максим Русанов <g.frimen@mail.ru>
2024-04-02 14:43:53 +0700

    Added an LED1 flasher

--- Src/main.c
+++ Src/main.c
@@ -97,6 +97,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+    HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
